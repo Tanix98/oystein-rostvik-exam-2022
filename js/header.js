2b602f0a26801a2b9f676d9a0dbf8441358791b3
Tanix <91118560+Tanix98@users.noreply.github.com>
2022-05-20 19:07:56 +0200

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -2,11 +2,18 @@ const hamburgerButton = document.querySelector(".hamburger-button");
 
 const mobileNav = document.querySelector(".mobile-nav");
 
+const blogNav = document.querySelector(".page-nav-mobile");
+
 hamburgerButton.addEventListener("click", function(){
-    if (mobileNav.style.display === "flex") {
-        mobileNav.style.display = "none";
-    } else {
-        mobileNav.style.display = "flex";
+    try {
+        if (mobileNav.style.display === "flex") {
+            mobileNav.style.display = "none";
+            blogNav.style.display = "flex"
+        } else {
+            mobileNav.style.display = "flex";
+            blogNav.style.display = "none"
+        } 
+    } catch (error) {
+        errorMessage.innerHTML = `<p>An error has occurred!</p>`;
     }
-    console.log("button works");
 });
