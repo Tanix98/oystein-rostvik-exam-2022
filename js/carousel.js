81992be227dea18de8f9cef907c4f837127bc599
Tanix <91118560+Tanix98@users.noreply.github.com>
2022-05-25 13:35:17 +0200

    Add files via upload

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,6 @@
-const apiPosts1 = "https://www.tanix.one/wp-json/wp/v2/posts?per_page=2";
+const apiPosts1 = "https://www.tanix.one/wp-json/wp/v2/posts?_fields=id,title,_links,_embedded&_embed&per_page=2";
 
-const apiPosts2 = "https://www.tanix.one/wp-json/wp/v2/posts?per_page=2&offset=2";
+const apiPosts2 = "https://www.tanix.one/wp-json/wp/v2/posts?_fields=id,title,_links,_embedded&_embed&per_page=2&offset=2";
 
 const carouselContainer1 = document.querySelector("#slide-1");
 
@@ -15,7 +15,7 @@ async function fetchRecentBlogPosts1() {
         errorMessage.innerHTML = "";
 
         for(let i = 0; i < data.length; i++) {
-            carouselContainer1.innerHTML += `<div class="blog-post"><a href="pages/blogpage.html?id=${data[i].id}" aria-label="Blog post: ${data[i].title.rendered}"><img src="${data[i].featured_media_src_url}" class="blog-post-thumbnail"> <h3 class="blog-title">${data[i].title.rendered}</h3> <p class="blog-date">${data[i].date}<p/></a></div>`;
+            carouselContainer1.innerHTML += `<div class="blog-post"><a href="pages/blogpage.html?id=${data[i].id}" aria-label="Blog post: ${data[i].title.rendered}"><img src="${data[i]._embedded['wp:featuredmedia']['0'].media_details.sizes.medium_large.source_url}" class="blog-post-thumbnail" alt="${data[i]._embedded['wp:featuredmedia']['0'].alt_text}"> <h3 class="blog-title">${data[i].title.rendered}</h3> <p class="blog-date">${data[i]._embedded['wp:featuredmedia']['0'].date}<p/></a></div>`;
         }
     }
     catch(error) {
@@ -33,7 +33,7 @@ async function fetchRecentBlogPosts2() {
         errorMessage.innerHTML = "";
 
         for(let i = 0; i < data.length; i++) {
-            carouselContainer2.innerHTML += `<div class="blog-post"><a href="pages/blogpage.html?id=${data[i].id}" aria-label="Blog post: ${data[i].title.rendered}"><img src="${data[i].featured_media_src_url}" class="blog-post-thumbnail"> <h3 class="blog-title">${data[i].title.rendered}</h3> <p class="blog-date">${data[i].date}<p/></a></div>`;
+            carouselContainer2.innerHTML += `<div class="blog-post"><a href="pages/blogpage.html?id=${data[i].id}" aria-label="Blog post: ${data[i].title.rendered}"><img src="${data[i]._embedded['wp:featuredmedia']['0'].media_details.sizes.medium_large.source_url}" class="blog-post-thumbnail" alt="${data[i]._embedded['wp:featuredmedia']['0'].alt_text}"> <h3 class="blog-title">${data[i].title.rendered}</h3> <p class="blog-date">${data[i]._embedded['wp:featuredmedia']['0'].date}<p/></a></div>`;
         }
     }
     catch(error) {
